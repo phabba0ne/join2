98942c50bb19a14f692fa7fed736aa93183b6d42
WebZenSai <fabian.zeussel@gmx.de>
2025-11-14 06:54:03 +0100

    fix: fallback version fabian

---
+++ src/app/board/add-task-mobile/add-task-mobile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-mobile',
+  imports: [],
+  templateUrl: './add-task-mobile.html',
+  styleUrl: './add-task-mobile.scss',
+})
+export class AddTaskMobile {
+
+}
